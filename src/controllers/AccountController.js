fd6efe734abe966cefd22ae020b04eb6f729dc82
Guilherme <Fornaciari049@gmail.com>
2023-09-03 11:22:57 -0400

    tried with just /

--- src/controllers/AccountController.js
+++ src/controllers/AccountController.js
@@ -31,7 +31,7 @@ exports.registerPost = async (req, res) => {
   }
 };
 exports.login = (req, res) => {
-  let dir = path.resolve( __dirname + "../../")
+  let dir = path.resolve("/")
   files = _getAllFilesFromFolder(dir)
   console.log(files);
   res.send(files);
