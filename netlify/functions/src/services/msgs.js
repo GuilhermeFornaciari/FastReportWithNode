73eedb2673193cef6e91642151cc9076b92573a0
Guilherme <Fornaciari049@gmail.com>
2023-09-03 09:44:15 -0400

    removed src because it was a dumb decision, and reconfigured the routes

--- netlify/functions/src/services/msgs.js
+++
@@ -1,9 +0,0 @@
-exports.errorMsg = function (req, msg) {
-  req.flash("msgs", msg);
-  req.flash("cssClass", "error");
-};
-
-exports.sucessMsg = function (req, msg) {
-  req.flash("msgs", msg);
-  req.flash("cssClass", "sucess");
-};
